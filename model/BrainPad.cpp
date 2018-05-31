33e5add9fd2e48509c1205b211ca1d6130985725
Michal Moskal <mimoskal@microsoft.com>
2018-05-30 18:06:31 -0700

    Copy model from brainpad for testing

---
+++ model/BrainPad.cpp
@@ -0,0 +1,108 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 Lancaster University, UK.
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+
+
+#include "BrainPad.h"
+#include "Timer.h"
+
+using namespace codal;
+
+static BrainPad *device_instance = NULL;
+
+/**
+  * Constructor.
+  *
+  * Create a representation of a GenuinoZero device, which includes member variables
+  * that represent various device drivers used to control aspects of the micro:bit.
+  */
+BrainPad::BrainPad() :
+    timer(),
+    messageBus(),
+    io()
+{
+    // Clear our status
+    status = 0;
+
+    device_instance = this;
+}
+
+void cpu_init();
+
+/**
+  * Post constructor initialisation method.
+  *
+  * This call will initialised the scheduler, memory allocator and Bluetooth stack.
+  *
+  * This is required as the Bluetooth stack can't be brought up in a
+  * static context i.e. in a constructor.
+  *
+  * @code
+  * uBit.init();
+  * @endcode
+  *
+  * @note This method must be called before user code utilises any functionality
+  *       contained within the GenuinoZero class.
+  */
+int BrainPad::init()
+{
+    if (status & DEVICE_INITIALIZED)
+        return DEVICE_NOT_SUPPORTED;
+
+    status |= DEVICE_INITIALIZED;
+
+    cpu_init();
+
+    timer.init();
+
+    // Bring up fiber scheduler.
+    scheduler_init(messageBus);
+
+    for(int i = 0; i < DEVICE_COMPONENT_COUNT; i++)
+    {
+        if(CodalComponent::components[i])
+            CodalComponent::components[i]->init();
+    }
+
+    // Seed our random number generator
+    //seedRandom();
+
+    codal_dmesg_set_flush_fn(brainpad_dmesg_flush);
+    status |= DEVICE_COMPONENT_STATUS_IDLE_TICK;
+
+    return DEVICE_OK;
+}
+
+/**
+  * A periodic callback invoked by the fiber scheduler idle thread.
+  * We use this for any low priority, backgrounf housekeeping.
+  *
+  */
+void BrainPad::idleCallback()
+{
+    codal_dmesg_flush();
+}
+
+void brainpad_dmesg_flush()
+{
+}
---
+++ model/BrainPadIO.cpp
@@ -0,0 +1,58 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2017 Lancaster University.
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+
+/**
+  * Class definition for MicroBit NEXT IO.
+  * Represents a collection of all I/O pins on the device.
+  */
+
+#include "CodalConfig.h"
+#include "BrainPadIO.h"
+
+using namespace codal;
+
+/**
+  * Constructor.
+  *
+  * Create a representation of all given I/O pins on the edge connector
+  *
+  * Accepts a sequence of unique ID's used to distinguish events raised
+  * by MicroBitPin instances on the default EventModel.
+  */
+BrainPadIO::BrainPadIO() :
+    scl (ID_PIN_SCL, PB_6, PIN_CAPABILITY_AD),
+    sda (ID_PIN_SDA, PB_7, PIN_CAPABILITY_AD),
+    buttonUp (ID_PIN_BUTTON_UP, PA_5, PIN_CAPABILITY_DIGITAL),
+    buttonDown (ID_PIN_BUTTON_DOWN, PB_10, PIN_CAPABILITY_DIGITAL),
+    buttonLeft (ID_PIN_BUTTON_LEFT, PA_15, PIN_CAPABILITY_DIGITAL),
+    buttonRight (ID_PIN_BUTTON_RIGHT, PC_13, PIN_CAPABILITY_DIGITAL),
+    ledGreen (ID_PIN_LED_GREEN, PC_8, PIN_CAPABILITY_AD),
+    ledBlue (ID_PIN_LED_BLUE, PC_6, PIN_CAPABILITY_AD),
+    ledRed (ID_PIN_LED_RED, PC_9, PIN_CAPABILITY_AD),
+    buzzer (ID_PIN_BUZZER, PB_8, PIN_CAPABILITY_AD),
+    interrupt (ID_PIN_INT, PC_14, PIN_CAPABILITY_AD),
+    servoOne (ID_PIN_SERVO_ONE, PA_3, PIN_CAPABILITY_AD),
+    servoTwo (ID_PIN_SERVO_TWO, PA_0, PIN_CAPABILITY_AD)
+{
+}
